c9f57150daf4df0625c054ab0a4ea65d59a44d61
sakamarupo <u15g033n@wako.ac.jp>
2018-01-10 15:31:47 +0900

    Add files via upload
    up

---
+++ PORTFOLIOSITE/js/script2.js
@@ -0,0 +1,12 @@
+<script>
+$(function(){
+	$('body').fadeMover({
+		'effectType': 1,
+		'inSpeed': 800,
+		'outSpeed': 800,
+		'inDelay' : '0',
+		'outDelay' : '0',
+		'nofadeOut' : 'nonmover'
+	});
+});
+</script>
